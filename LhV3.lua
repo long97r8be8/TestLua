16ef760bf7c7ed296728b0c71efb987d1de8d94c
long97r8be8 <Segaykobro01873@gmail.com>
2024-08-27 19:40:42 +0700

    Add files via upload

--- LhV3.lua
+++ LhV3.lua
@@ -2479,3 +2479,66 @@ local Button1 = Section:Button({
         setclipboard("https://discord.com/invite/7ktB8D87")
     end
 })
+local Section = MainTab:Section({
+    ["Title"] = "Farm Main",
+    ["Content"] = "Farm Farm"
+})
+local Seperator = Section:Seperator("Farm Main")
+local Paragraph = Section:Paragraph({
+    ["Title"] = "Fast Attack",
+    ["Content"] = "select Fast Attack"
+})
+    local DropdownDelayAttack = Section:Dropdown("DropdownDelayAttack", {
+    ["Title"] = "Select Fast Attack",
+    ["Multi"] = false,
+    ["Options"] = {"Normal Attack", "Fast Attack", "LongSieuVip"},
+    ["Default"] = 1,
+    ["Place Holder Text"] = "Select Fast Attack",
+    })
+    DropdownDelayAttack:SetValue("Fast Attack")
+    DropdownDelayAttack:OnChanged(function(Value)
+    _G.FastAttackGamer144_Mode = Value
+	if _G.FastAttackGamer144_Mode == "Fast Attack" then
+		_G.Fast_Delay = 0.03
+	elseif _G.FastAttackGamer144_Mode == "Normal Attack" then
+		_G.Fast_Delay = 0.02
+	elseif _G.FastAttackGamer144YTB_Mode == "LongSieuVip" then
+	    coroutine.wrap(function()
+local StopCamera = require(game.ReplicatedStorage.Util.CameraShaker)StopCamera:Stop()
+    for v,v in pairs(getreg()) do
+        if typeof(v) == "function" and getfenv(v).script == game:GetService("Players").LocalPlayer.PlayerScripts.CombatFramework then
+             for v,v in pairs(debug.getupvalues(v)) do
+                if typeof(v) == "table" then
+                    spawn(function()
+                        game:GetService("RunService").RenderStepped:Connect(function()
+                            if getgenv().Config['FastAttack'] then
+                                 pcall(function()
+                                     v.activeController.timeToNextAttack = -(math.huge^math.huge^math.huge)
+                                     v.activeController.attacking = false
+                                     v.activeController.increment = 4
+                                     v.activeController.blocking = false   
+                                     v.activeController.hitboxMagnitude = 150
+    		                         v.activeController.humanoid.AutoRotate = true
+    	                      	     v.activeController.focusStart = 0
+    	                      	     v.activeController.currentAttackTrack = 0
+                                     sethiddenproperty(game:GetService("Players").LocalPlayer, "SimulationRaxNerous", math.huge)
+                                 end)
+                             end
+                         end)
+                    end)
+                end
+            end
+        end
+    end
+end)();
+
+spawn(function()
+    game:GetService("RunService").RenderStepped:Connect(function()
+        if getgenv().Config['ClickAttack'] then
+             pcall(function()
+                game:GetService'VirtualUser':CaptureController()
+			    game:GetService'VirtualUser':Button1Down(Vector2.new(0,1,0,1))
+            end)
+        end
+    end)
+end)
